35e7429d173250557fedd2cfc9099030d7699528
Thomas Galliker <thomas@bluewin.ch>
2021-12-10 14:45:42 +0100

    Reformat code

--- Http.Resilience/Internals/NetworkHelper.cs
+++ Http.Resilience/Internals/NetworkHelper.cs
@@ -45,23 +45,37 @@ namespace Http.Resilience.Internals
         {
             if (ex is WebException webException)
             {
-                if (webException.Response != null && webException.Response is HttpWebResponse response)
+                if (webException.Response is HttpWebResponse httpWebResponse)
                 {
-                    var httpWebResponse = response;
                     if (options.RetryableStatusCodes.Contains(httpWebResponse.StatusCode))
                     {
                         return true;
                     }
                 }
 
-                if (webException.Status == WebExceptionStatus.ConnectFailure || webException.Status == WebExceptionStatus.ConnectionClosed || webException.Status == WebExceptionStatus.KeepAliveFailure || webException.Status == WebExceptionStatus.NameResolutionFailure || webException.Status == WebExceptionStatus.ReceiveFailure || webException.Status == WebExceptionStatus.SendFailure || webException.Status == WebExceptionStatus.Timeout)
+                if (webException.Status == WebExceptionStatus.ConnectFailure ||
+                    webException.Status == WebExceptionStatus.ConnectionClosed || 
+                    webException.Status == WebExceptionStatus.KeepAliveFailure ||
+                    webException.Status == WebExceptionStatus.NameResolutionFailure || 
+                    webException.Status == WebExceptionStatus.ReceiveFailure ||
+                    webException.Status == WebExceptionStatus.SendFailure ||
+                    webException.Status == WebExceptionStatus.Timeout)
                 {
                     return true;
                 }
             }
             else if (ex is SocketException ex3)
             {
-                if (ex3.SocketErrorCode == SocketError.Interrupted || ex3.SocketErrorCode == SocketError.NetworkDown || ex3.SocketErrorCode == SocketError.NetworkUnreachable || ex3.SocketErrorCode == SocketError.NetworkReset || ex3.SocketErrorCode == SocketError.ConnectionAborted || ex3.SocketErrorCode == SocketError.ConnectionReset || ex3.SocketErrorCode == SocketError.TimedOut || ex3.SocketErrorCode == SocketError.HostDown || ex3.SocketErrorCode == SocketError.HostUnreachable || ex3.SocketErrorCode == SocketError.TryAgain)
+                if (ex3.SocketErrorCode == SocketError.Interrupted ||
+                    ex3.SocketErrorCode == SocketError.NetworkDown ||
+                    ex3.SocketErrorCode == SocketError.NetworkUnreachable ||
+                    ex3.SocketErrorCode == SocketError.NetworkReset ||
+                    ex3.SocketErrorCode == SocketError.ConnectionAborted ||
+                    ex3.SocketErrorCode == SocketError.ConnectionReset || 
+                    ex3.SocketErrorCode == SocketError.TimedOut ||
+                    ex3.SocketErrorCode == SocketError.HostDown || 
+                    ex3.SocketErrorCode == SocketError.HostUnreachable ||
+                    ex3.SocketErrorCode == SocketError.TryAgain)
                 {
                     return true;
                 }
@@ -72,7 +86,12 @@ namespace Http.Resilience.Internals
                 if (nativeErrorCode > 12000 && nativeErrorCode <= 12188)
                 {
                     var winHttpErrorCode = (WinHttpErrorCode)nativeErrorCode;
-                    if (winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_CANNOT_CONNECT || winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_CONNECTION_ERROR || winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_INTERNAL_ERROR || winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_NAME_NOT_RESOLVED || winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_SECURE_FAILURE || winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_TIMEOUT)
+                    if (winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_CANNOT_CONNECT ||
+                        winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_CONNECTION_ERROR ||
+                        winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_INTERNAL_ERROR ||
+                        winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_NAME_NOT_RESOLVED || 
+                        winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_SECURE_FAILURE ||
+                        winHttpErrorCode == WinHttpErrorCode.ERROR_WINHTTP_TIMEOUT)
                     {
                         return true;
                     }
@@ -96,7 +115,19 @@ namespace Http.Resilience.Internals
             else if (ex.GetType().Name == "CurlException" && ex.HResult > 0 && ex.HResult < 94)
             {
                 var curlErrorCode = (CurlErrorCode)ex.HResult;
-                if (curlErrorCode == CurlErrorCode.CURLE_COULDNT_RESOLVE_PROXY || curlErrorCode == CurlErrorCode.CURLE_COULDNT_RESOLVE_HOST || curlErrorCode == CurlErrorCode.CURLE_COULDNT_CONNECT || curlErrorCode == CurlErrorCode.CURLE_HTTP2 || curlErrorCode == CurlErrorCode.CURLE_PARTIAL_FILE || curlErrorCode == CurlErrorCode.CURLE_WRITE_ERROR || curlErrorCode == CurlErrorCode.CURLE_UPLOAD_FAILED || curlErrorCode == CurlErrorCode.CURLE_READ_ERROR || curlErrorCode == CurlErrorCode.CURLE_OPERATION_TIMEDOUT || curlErrorCode == CurlErrorCode.CURLE_INTERFACE_FAILED || curlErrorCode == CurlErrorCode.CURLE_GOT_NOTHING || curlErrorCode == CurlErrorCode.CURLE_SEND_ERROR || curlErrorCode == CurlErrorCode.CURLE_RECV_ERROR)
+                if (curlErrorCode == CurlErrorCode.CURLE_COULDNT_RESOLVE_PROXY ||
+                    curlErrorCode == CurlErrorCode.CURLE_COULDNT_RESOLVE_HOST ||
+                    curlErrorCode == CurlErrorCode.CURLE_COULDNT_CONNECT || 
+                    curlErrorCode == CurlErrorCode.CURLE_HTTP2 ||
+                    curlErrorCode == CurlErrorCode.CURLE_PARTIAL_FILE || 
+                    curlErrorCode == CurlErrorCode.CURLE_WRITE_ERROR || 
+                    curlErrorCode == CurlErrorCode.CURLE_UPLOAD_FAILED ||
+                    curlErrorCode == CurlErrorCode.CURLE_READ_ERROR || 
+                    curlErrorCode == CurlErrorCode.CURLE_OPERATION_TIMEDOUT || 
+                    curlErrorCode == CurlErrorCode.CURLE_INTERFACE_FAILED ||
+                    curlErrorCode == CurlErrorCode.CURLE_GOT_NOTHING || 
+                    curlErrorCode == CurlErrorCode.CURLE_SEND_ERROR ||
+                    curlErrorCode == CurlErrorCode.CURLE_RECV_ERROR)
                 {
                     return true;
                 }
