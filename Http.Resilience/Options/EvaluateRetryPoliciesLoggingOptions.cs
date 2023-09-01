70d5c6b25b33fcf54a5c0ae14b40a6a3f3a62fd1
Thomas Galliker <thomas@bluewin.ch>
2023-09-01 14:20:44 +0200

    Update logging options

--- Http.Resilience/Options/EvaluateRetryPoliciesLoggingOptions.cs
+++ Http.Resilience/Options/EvaluateRetryPoliciesLoggingOptions.cs
@@ -4,33 +4,30 @@ namespace Http.Resilience
 {
     public class EvaluateRetryPoliciesLoggingOptions
     {
-        public EvaluateRetryPoliciesLoggingOptions(
-            string shouldRetryTrue,
-            string shouldRetryFalse,
-            string shouldRetryUnknown)
+        public EvaluateRetryPoliciesLoggingOptions()
         {
-            this.ShouldRetry = shouldRetryTrue;
-            this.ShouldNotRetry = shouldRetryFalse;
-            this.NotEvaluated = shouldRetryUnknown;
         }
 
         /// <summary>
         /// The check mark string to be used in log messages
         /// when <see cref="IRetryPolicy.ShouldRetry(object)"/> returned <c>true</c>.
+        /// Default: "X" (check mark).
         /// </summary>
-        public string ShouldRetry { get; set; }
+        public string ShouldRetry { get; set; } = "X";
 
         /// <summary>
         /// The check mark string to be used in log messages
         /// when <see cref="IRetryPolicy.ShouldRetry(object)"/> returned <c>false</c>.
+        /// Default: "-" (dash).
         /// </summary>
-        public string ShouldNotRetry { get; set; }
+        public string ShouldNotRetry { get; set; } = "-";
 
         /// <summary>
         /// The check mark string to be used in log messages
         /// when <see cref="IRetryPolicy.ShouldRetry(object)"/> was not evaluated
         /// since another retry policy has returned <c>true</c>.
+        /// Default: " " (white space).
         /// </summary>
-        public string NotEvaluated { get; set; }
+        public string NotEvaluated { get; set; } = " ";
     }
 }
--- Http.Resilience/Options/LoggingOptions.cs
+++ Http.Resilience/Options/LoggingOptions.cs
@@ -2,6 +2,6 @@
 {
     public class LoggingOptions
     {
-        public EvaluateRetryPoliciesLoggingOptions EvaluateRetryPolicies { get; set; } = new EvaluateRetryPoliciesLoggingOptions("X", "-", " ");
+        public EvaluateRetryPoliciesLoggingOptions EvaluateRetryPolicies { get; set; } = new EvaluateRetryPoliciesLoggingOptions();
     }
 }
